bef975c20fe9c5337fe9114b6bcef5b337e4179f
m431m <m431m@tutanota.com>
2019-01-30 16:01:39 +0100

    Updated copyright

--- onegeo_manager/protocol/wfs.py
+++ onegeo_manager/protocol/wfs.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2018 Neogeo-Technologies.
+# Copyright (c) 2017-2019 Neogeo-Technologies.
 # All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
